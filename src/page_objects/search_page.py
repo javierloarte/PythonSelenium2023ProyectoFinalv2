e006ec706cbb31c6debc6c520995869d8824c86a
javierloarte <jall87@hotmail.com>
2023-05-30 18:29:50 -0500

    Initial commit

--- src/page_objects/search_page.py
+++ src/page_objects/search_page.py
@@ -1,12 +1,13 @@
 import logging
 
-
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 from src.page_objects.base_page import BasePage
+from src.page_objects.home_page import SearchPage
 
-class search_item(BasePage):
+
+class buscar_item(BasePage):
     def __init__(self, driver: WebDriver, wait_driver: WebDriverWait):
         super(SearchPage, self).__init__(driver, wait_driver)
 
@@ -14,5 +15,3 @@ class search_item(BasePage):
         logging.info(f"Search {value}")
         self.element("search_input").wait_clickable().send_keys(value)
         self.element("search_btn").wait_clickable().click()
-
-
